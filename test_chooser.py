72a586ccab06771fc0d840def57ca728e8bd91dc
Giedrelis <58008054+Giedrelis@users.noreply.github.com>
2023-10-19 08:41:59 +0300

    update

--- test_chooser.py
+++ test_chooser.py
@@ -2,48 +2,106 @@ import streamlit as st
 import json
 
 # Load the decision tree from the JSON file
-@st.cache
-def load_decision_tree():
-    with open('decision_tree.json', 'r') as file:
-        return json.load(file)
+try:
+    with open("decision_tree.json", "r", encoding="utf-8") as file:
+        DECISION_TREE = json.load(file)
+except FileNotFoundError:
+    st.error("The decision_tree.json file was not found.")
+    st.stop()
+except json.JSONDecodeError:
+    st.error("Invalid JSON format in decision_tree.json.")
+    st.stop()
+except Exception as e:
+    st.error(f"An error occurred: {e}")
+    st.stop()
 
-decision_tree = load_decision_tree()
+def get_next_node(node_id, tree):
+    """
+    Retrieve the node with the given ID from the decision tree.
+    """
+    for node in tree:
+        if node["id"] == node_id:
+            return node
+        if "nodes" in node:
+            next_node = get_next_node(node_id, node["nodes"])
+            if next_node:
+                return next_node
+    return None
 
-# Recursive function to navigate through the decision tree
-def navigate_tree(node):
-    # Display the question
-    st.write(node["question"])
+def chat_with_tree():
+    """
+    Chat with the user based on the decision tree.
+    """
+    st.title("ČekanavičiusGPT")
 
-    # If there's a comment, display it
-    if "comments" in node:
-        st.write(node["comments"])
+    # Apply custom styles
+    st.markdown("""
+        <style>
+            .stChat .stChatMessage p {
+                font-size: 20px;
+            }
+            .stButton>button {
+                width: 100%;
+            }
+        </style>
+    """, unsafe_allow_html=True)
 
-    # Display the answers as buttons
-    answer = st.radio("", [ans["text"] for ans in node["answers"]])
+    # Initialize or retrieve the current node and chat history from session state
+    if "current_node" not in st.session_state:
+        st.session_state.current_node = DECISION_TREE
+        st.session_state.chat_history = []
+        st.session_state.chat_history.append({"name": "assistant", "text": DECISION_TREE["question"]})
+        if "comments" in DECISION_TREE:
+            st.session_state.chat_history.append({"name": "assistant", "text": DECISION_TREE["comments"]})
+    current_node = st.session_state.current_node
 
-    # Find the selected answer
-    for ans in node["answers"]:
-        if ans["text"] == answer:
-            selected_answer = ans
+    # Display the chat history
+    for message in st.session_state.chat_history:
+        with st.chat_message(name=message["name"]):
+            st.write(message["text"])
+
+    # Display the available answers as buttons
+    selected_option = None
+    for option in [answer["text"] for answer in current_node["answers"]]:
+        if st.button(option):
+            selected_option = option
             break
 
-    # If the selected answer has an action, display it
-    if "action" in selected_answer:
-        st.write(selected_answer["action"])
-        if "interpretation" in selected_answer:
-            st.write(selected_answer["interpretation"])
+    if selected_option:
+        with st.chat_message("user"):
+            st.write(selected_option)
 
-    # If the selected answer leads to another node, navigate to that node
-    elif "next" in selected_answer:
-        next_node_id = selected_answer["next"]
-        for next_node in node.get("nodes", []):
-            if next_node["id"] == next_node_id:
-                navigate_tree(next_node)
-                break
+        # Add the selected answer to the chat history
+        st.session_state.chat_history.append({"name": "user", "text": selected_option})
+
+        # Find the selected answer
+        for answer in current_node["answers"]:
+            if answer["text"] == selected_option:
+                if "action" in answer:
+                    with st.chat_message(name="assistant"):
+                        st.write(answer["action"])
+                    # Add the action to the chat history
+                    st.session_state.chat_history.append({"name": "assistant", "text": answer["action"]})
+                elif "next" in answer:
+                    next_node = get_next_node(answer["next"], [DECISION_TREE])
+                    st.session_state.chat_history.append({"name": "assistant", "text": next_node["question"]})
+                    if "comments" in next_node:
+                        st.session_state.chat_history.append({"name": "assistant", "text": next_node["comments"]})
+                    st.session_state.current_node = next_node
+                    st.experimental_rerun()
+
+    # Reset button on the bottom right, always visible
+    cols = st.columns([4, 1])
+    with cols[1]:
+        if st.button("Iš naujo", type="primary"):
+            if "current_node" in st.session_state:
+                del st.session_state.current_node
+            if "chat_history" in st.session_state:
+                del st.session_state.chat_history
+            st.experimental_rerun()
 
 def main():
-    st.title("ČekanavičiusGPT")
-    navigate_tree(decision_tree)
+    chat_with_tree()
 
 if __name__ == "__main__":
     main()
